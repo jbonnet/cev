f4cd2f30a5acf82d9547999072afa3e4860d8b69
José Bonnet <jbonnet1@mac.com>
2013-04-08 12:14:42 +0100

    First, empty version

---
+++ spec/app.spec.js
@@ -0,0 +1,30 @@
+describe("Chemical Equation Validator Page", function() {
+  it("should have two sides on the equation", function() {
+  });
+
+  describe("each side of the equation", function(){
+    it("should have at least one composit", function() {
+    });
+
+    describe("composite factors", function(){
+      it("should have a factor", function(){
+      });
+      it("factors by default should be one", function(){
+      });
+    });
+
+    describe("composite elements", function() {
+      it("should have at least one element", function(){
+      });
+      it("each element should have a number of atoms", function(){
+      });
+      it("the number of atoms by default should be one", function(){
+      });
+    });
+  });
+
+  describe("a valid equation", function(){
+    it("should have the same number of elements on each side of the equation", function() {
+    });
+  });
+});
